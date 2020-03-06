15fa96449d6dee9fdc9475966a4743744fa4bdcb
Yucheng Chen <chenyucheng2011@gmail.com>
2020-03-06 09:38:35 -0500

    correct hist

--- hw1/problem2.py
+++ hw1/problem2.py
@@ -14,7 +14,8 @@ def SampleMethod1(numSamples):
 		elif theta == 1:
             		for i in range(5):
                 		y.append(bernoulli.rvs(3.0/4.0, size = 1))
-		ys.append(y)
+		print(y)
+		ys.append(np.sum(y))
 	return (thetas, ys)
 
 def SampleMethod2(numSamples):
@@ -31,7 +32,7 @@ def SampleMethod2(numSamples):
 				sample = bernoulli.rvs(3.0/4.0, size = 1)
 			y.append(sample)
 			p = p*bernoulli.pmf(sample, 3.0/4.0)/(p*bernoulli.pmf(sample,3.0/4.0) +(1 - p)*bernoulli.pmf(sample,1.0/2.0))
-		ys.append(y)
+		ys.append(np.sum(y))
 		thetas.append(bernoulli.rvs(p,size = 1))
 	return (thetas, ys)
 
@@ -40,16 +41,16 @@ if __name__== "__main__":
     numSamples = 10000
     thetas1, ys1 = SampleMethod1(numSamples)
     thetas2, ys2 = SampleMethod2(numSamples)
-    ys_np1 = np.sum(np.array(ys1),axis = 1)
-    ys_np2 = np.sum(np.array(ys2),axis = 1)
+    #ys_np1 = np.sum(np.array(ys1),axis = 1)
+    #ys_np2 = np.sum(np.array(ys2),axis = 1)
     plt.figure()
     ax1 = plt.subplot(211)
     ax1.set_title("Method1")
-    plt.hist(ys_np1,bins = 5)
-   # plt.hist(np.array(thetas1), bins = 2)
+    plt.hist(ys1,bins = 6)
+    #plt.hist(np.array(thetas1), bins = 2)
     ax2 = plt.subplot(212)
     ax2.set_title("Method2")
-    plt.hist(ys_np2, bins = 5)
+    plt.hist(ys2, bins = 6)
     #plt.hist(np.array(thetas2), bins = 2)
     plt.show()
 
